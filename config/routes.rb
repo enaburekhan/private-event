72a2796935cb4eaa140cde83c88017420a54a607
enaburekhan <ericenaburekhan2018@gmail.com>
2023-07-20 20:18:39 +0100

    Create user model using devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users
+  
   root 'events#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :events, only: [:new, :create, :index]
 end
