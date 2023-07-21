3e1e9ece802153caec110f47f3e5d470ce781c90
enaburekhan <ericenaburekhan2018@gmail.com>
2023-07-21 15:02:49 +0100

    Create association between User and Event model

---
+++ app/controllers/events_controller.rb
@@ -0,0 +1,27 @@
+class EventsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
+
+  def index
+    @events = Event.all
+  end
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @event = Event.new(event_params)
+
+    if @event.save
+      redirect_to events_url
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end 
+  
+  private 
+
+    def event_params
+      params.require(:event).permit(:name, :date, :location)
+    end
+end
